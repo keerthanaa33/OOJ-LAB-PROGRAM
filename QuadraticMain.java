adbbee464e4e7e170d520c0b0b7fc3b8614928ba
keerthanaa33 <keerthanabhat.cs23@bmsce.ac.in>
2024-09-30 15:17:07 +0530

    Create QuadraticMain.java

---
+++ QuadraticMain.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.lang.Math; 
+
+class Quadratic{
+	int a,b,c;
+	double root1,root2,realpart,imagpart,d;
+	
+
+	void getd()
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the value of a");
+	a=sc.nextInt();
+	System.out.println("Enter the value of b");
+	b=sc.nextInt();
+	System.out.println("Enter the value of c");
+	c=sc.nextInt();
+}
+
+	void compute()
+{
+	d=b*b-4*a*c;
+	if(d==0)
+{
+	root1=root2=-b/(2*a);
+	System.out.println("Roots are equal");
+	System.out.println("Root1="+root1);
+	System.out.println("Root2="+root2);
+}
+	else if(d>0)
+{	System.out.println("Roots are real and different");
+
+	root1=(-b+Math.sqrt(d))/(2.0*a);
+	root2=(-b-Math.sqrt(d))/(2.0*a);
+
+	System.out.println("Root1="+root1);
+	System.out.println("Root2="+root2);
+}
+	else if(d<0)
+{
+	System.out.println("Roots are imaginary");
+	realpart=-b/(2*a);
+	imagpart = Math.sqrt(-d) / (2.0 * a);
+	System.out.println("Root1= " + realpart + " - " + imagpart + "i");
+	System.out.println("Root2= " + realpart + " - " + imagpart + "i");
+ 
+}
+}}
+class QuadraticMain{
+
+	public static void main(String args[])
+{
+	Quadratic q=new Quadratic();
+	q.getd();
+	q.compute();
+	
+}
+}
